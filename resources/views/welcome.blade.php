b61d68005536a0fce1f674e18b336b4791e75edd
dani03 <lesamessipa@gmail.com>
2021-04-17 14:19:12 +0200

    remove button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,9 +22,7 @@
     </head>
     <body class="antialiased">
         <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-            <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                <a href="{{url('/projects')}}"></a> go to Projects
-               </button>
+            <a href="{{route('projects.index')}}"> projects</a>
             <div class="relative py-3 sm:max-w-xl sm:mx-auto">
               <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
               <div class="relative px-4 py-10  bg-white shadow-lg sm:rounded-3xl sm:p-20">
