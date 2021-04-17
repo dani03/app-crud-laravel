7d183d5768e17a4dd6c6a9edd559f96f8839d50e
dani03 <lesamessipa@gmail.com>
2021-04-17 17:48:57 +0200

    updated title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,8 +22,9 @@
     </head>
     <body class="antialiased">
         <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-            <a href="{{route('projects.index')}}"> projects</a>
+           
             <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+                <a href="{{route('projects.index')}}"> projects</a>
               <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
               <div class="relative px-4 py-10  bg-white shadow-lg sm:rounded-3xl sm:p-20">
                   <div class="flex absolute top-1 mt-8 gap-10 justify-center left-36">
