420711fdcabb43011fb141f796ff964e6fa6a866
dani03 <lesamessipa@gmail.com>
2021-04-17 10:54:47 +0200

    add button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,9 @@
     </head>
     <body class="antialiased">
         <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+            <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                <a href="{{url('/projects')}}"></a> go to Projects
+               </button>
             <div class="relative py-3 sm:max-w-xl sm:mx-auto">
               <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
               <div class="relative px-4 py-10  bg-white shadow-lg sm:rounded-3xl sm:p-20">
@@ -38,11 +41,7 @@
             </div>
           </div>
           
-          <style>
-              .border-8{
-                  border-width: 20px;
-              }
-          </style>
+          
           
           <!-- BUY ME A BEER AND HELP SUPPORT OPEN-SOURCE RESOURCES -->
           <div class="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
